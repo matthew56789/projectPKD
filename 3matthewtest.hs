5b5872f8cf40df36f323dcc8a31d1ba478650ca8
matthew andersson <matthew.a01@gmail.com>
2022-03-01 19:43:30 +0100

    testuing

--- 3matthewtest.hs
+++ 3matthewtest.hs
@@ -18,13 +18,24 @@ printScrapeResults (Just results) = forM_ results print
 -}
 
 
+main :: String -> IO (Maybe [String])
+main = do
+	url <- getLine
+	let scrapeResults = scrapeURL url pricetag
+  	printScrapeResults scrapeResults
 
+printScrapeResults Nothing = putStrLn "Something went wrong!"
+printScrapeResults (Just []) = putStrLn "Couldn't scrape anything!"
+printScrapeResults (Just results) = forM_ results print
 
-scrapper :: String -> IO (Maybe [String])
-scrapper url = scrapeURL url pricetag
+--scrapper :: String -> IO (Maybe [String])
+--scrapper url = scrapeURL url pricetag
+
+removeJust :: Maybe a -> a
+removeJust (Just x) = x
 
 pricetag :: Scraper String [String]
-pricetag = texts $ "div" @: [hasClass "price-box"]
+pricetag = texts $ "span" @: [hasClass "product-price-now"]
 	--texts $ "div" @: [hasClass "text"] -- $ do
 --	contents <- text anySelector
 --	guard ("p" `isInfixOf` contents)
@@ -47,6 +58,11 @@ pricetag = texts $ "div" @: [hasClass "price-box"]
 
 
 {--scrapper "https://www.mediamarkt.se/sv/product/_samsung-galaxy-s22-ultra-128gb-6-8-smartphone-black-1339393.html"
-
-
+price
+scrapper "https://www.komplett.se/product/1196420/mobil-klockor/mobiltelefoner/iphone-13-128gb-blue"
+product-price-now        span
+scrapper "https://www.newegg.com/cyberpowerpc-gamer-master-gm60900/p/N82E16883230646?Item=N82E16883230646&cm_sp=Homepage_SS-_-P2_83-230-646-_-03012022"
+li     price-current
+scrapper "https://www.netonnet.se/art/mobil-smartwatch/mobiltelefoner/samsung-mobil/samsung-galaxy-a32-5g-64gb-black/1016614.9050/"
+div     price-big    funkar ej
 -}
