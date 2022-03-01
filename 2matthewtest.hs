a5a47951b451153ba6944f5f50c67a480c717f34
Egon <henriksenegon2@gmail.com>
2022-03-01 19:41:42 +0100

    fetchPrice

--- 2matthewtest.hs
+++ 2matthewtest.hs
@@ -3,11 +3,14 @@
 import Text.HTML.Scalpel
 import Control.Monad
 import Control.Applicative
-
+-- import Network.HTTP.Conduit ( simpleHttp )
+-- import qualified Data.ByteString.Lazy.Char8 as CL
 
 main :: IO ()
 main = do
-  exampleHtml <- readFile "elgiganten.HTML"
+  -- testhtml <- simpleHttp "https://www.elgiganten.se/product/mobiler-tablets-smartklockor/mobiltelefon/oneplus-8-pro-smartphone-8128gb-onyx-black/162403"
+  -- let exampleHtml = CL.unpack testhtml
+  exampleHtml <- readFile "Elgiganten.html"
   let scrapeResults = scrapeStringLike exampleHtml altTextAndImages
   printScrapeResults scrapeResults
 
@@ -18,3 +21,9 @@ printScrapeResults (Just results) = forM_ results print
 altTextAndImages :: Scraper String [String]
 altTextAndImages = texts $ "div" @: [hasClass "feature__price"]
    -- chroots ("div" @: [hasClass "feature__price ng-star-inserted"]) (text "")
+
+-- main :: IO ()
+-- main = do 
+	-- testhtml <- simpleHttp "https://www.example.com/"
+	-- let exampleHtml = CL.unpack testhtml
+	-- putStrLn exampleHtml
